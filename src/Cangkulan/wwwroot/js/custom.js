98c01f350daaf9ad572b6d7b6c18f1520ce611ed
mifmasterz <mifmasterz@yahoo.com>
2022-11-13 13:37:10 +0700

    ok

--- src/Cangkulan/wwwroot/js/custom.js
+++ src/Cangkulan/wwwroot/js/custom.js
@@ -1295,9 +1295,101 @@ function InitJs() {
 	});
 
 
+	// Snackbar for user status switcher
+	$('#snackbar-user-status label').click(function () {
+		Snackbar.show({
+			text: 'Your status has been changed!',
+			pos: 'bottom-center',
+			showAction: false,
+			actionText: "Dismiss",
+			duration: 3000,
+			textColor: '#fff',
+			backgroundColor: '#383838'
+		});
+	});
 
+	function initAutocomplete() {
+		var options = {
+			types: ['(cities)'],
+			// componentRestrictions: {country: "us"}
+		};
+
+		var input = document.getElementById('autocomplete-input');
+		var autocomplete = new google.maps.places.Autocomplete(input, options);
+	}
 	// ------------------ End Document ------------------ //
 }
 
+function LoadChart() {
+	Chart.defaults.global.defaultFontFamily = "Nunito";
+	Chart.defaults.global.defaultFontColor = '#888';
+	Chart.defaults.global.defaultFontSize = '14';
 
+	var ctx = document.getElementById('chart').getContext('2d');
+
+	var chart = new Chart(ctx, {
+		type: 'line',
+
+		// The data for our dataset
+		data: {
+			labels: ["January", "February", "March", "April", "May", "June"],
+			// Information about the dataset
+			datasets: [{
+				label: "Views",
+				backgroundColor: 'rgba(42,65,232,0.08)',
+				borderColor: '#2a41e8',
+				borderWidth: "3",
+				data: [196, 132, 215, 362, 210, 252],
+				pointRadius: 5,
+				pointHoverRadius: 5,
+				pointHitRadius: 10,
+				pointBackgroundColor: "#fff",
+				pointHoverBackgroundColor: "#fff",
+				pointBorderWidth: "2",
+			}]
+		},
+
+		// Configuration options
+		options: {
+
+			layout: {
+				padding: 10,
+			},
+
+			legend: { display: false },
+			title: { display: false },
+
+			scales: {
+				yAxes: [{
+					scaleLabel: {
+						display: false
+					},
+					gridLines: {
+						borderDash: [6, 10],
+						color: "#d8d8d8",
+						lineWidth: 1,
+					},
+				}],
+				xAxes: [{
+					scaleLabel: { display: false },
+					gridLines: { display: false },
+				}],
+			},
+
+			tooltips: {
+				backgroundColor: '#333',
+				titleFontSize: 13,
+				titleFontColor: '#fff',
+				bodyFontColor: '#fff',
+				bodyFontSize: 13,
+				displayColors: false,
+				xPadding: 10,
+				yPadding: 10,
+				intersect: false
+			}
+		},
+
+
+	});
+}
 
